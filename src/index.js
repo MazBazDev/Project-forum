5f6e4d46532f95b5e5833ae954eb553a70aaeee5
MazBaz <mrlog42@gmail.com>
2023-06-10 07:58:12 +0200

    added 404page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +12,8 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/*" element={<App />} />
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   </React.StrictMode>,
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>404 - Page not found</h1>
+        <p>The requested page could not be found.</p>
+      </>
+    );
+  }
+}
+
+export default NotFoundPage;
