0926d6c25647ad010f60e3280274610d5345ad81
MazBaz <mrlog42@gmail.com>
2023-06-15 13:20:28 +0200

    added get post by cat

--- Controllers/Categories/Categories.go
+++ Controllers/Categories/Categories.go
@@ -3,6 +3,7 @@ package categories
 import (
 	"encoding/json"
 	"fmt"
+	comments "main/Controllers/Comments"
 	models "main/Models"
 	"net/http"
 	"strings"
@@ -105,4 +106,43 @@ func GetCategoriesByPostID(postID int) ([]models.Caterogy, error) {
 	}
 	fmt.Println("hohoho", categories)
 	return categories, nil
+}
+
+func GetPostsByCategory(categoryID int) ([]models.Post, error) {
+	var posts []models.Post
+
+	query := `
+		SELECT p.id, p.title, p.content, p.created_at, u.id, u.email, u.username, u.profile_picture, c.city, c.lat, c.long
+		FROM posts p
+		INNER JOIN users u ON p.user_id = u.id
+		INNER JOIN coordinates c ON p.id = c.post_id
+		INNER JOIN categories_post cp ON p.id = cp.post_id
+		WHERE cp.categorie_id = ?
+	`
+
+	rows, err := models.Database.Query(query, categoryID)
+	if err != nil {
+		return []models.Post{}, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var post models.Post
+		err := rows.Scan(&post.Id, &post.Title, &post.Content, &post.Created_at, &post.User.Id, &post.User.Email, &post.User.Username, &post.User.ProfilePicture, &post.Coordinates.City, &post.Coordinates.Lat, &post.Coordinates.Long)
+
+		if err != nil {
+			return []models.Post{}, err
+		}
+
+		comments, err := comments.GetCommentsByPostID(post.Id)
+		if err != nil {
+			fmt.Println("Controllers > posts ", err)
+			return []models.Post{}, err
+		}
+		post.Comments = comments
+
+		posts = append(posts, post)
+	}
+
+	return posts, nil
 }
