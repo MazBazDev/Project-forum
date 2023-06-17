492c3d208640101e3e1cac7fe44ccdb08a00b6ed
MazBaz <mrlog42@gmail.com>
2023-06-17 21:57:22 +0200

    fix indent

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -4,7 +4,7 @@ import Input from "../../components/Input";
 import axios from "axios";
 import Cookies from 'js-cookie'
 import Notiflix from 'notiflix';
-import {json, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
   const navigate = useNavigate();
